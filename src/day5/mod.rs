6e4f1e92fe451f4c92a3b0e4ac0573f0af4dbe01
apocalypseDreamer <damntheguitars@gmail.com>
2023-12-28 17:52:15 +0100

    day 5 initialize

---
+++ src/day5/mod.rs
@@ -0,0 +1,5 @@
+use crate::read::read_input_file;
+
+pub fn task1() {
+   let lines = read_input_file("day4");
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 mod day6;
 mod day7;
 mod day8;
@@ -18,9 +19,10 @@ fn main() {
     // day2::task2();
     // day3::task2();
     // day4::task2();
+    day5::task1();
     // day7::task2();
     // day8::task2();
     // day9::task2();
     // day10::task1();
-    day11::task1();
+    // day11::task1();
 }
