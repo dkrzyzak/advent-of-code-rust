f9dd4bae401ff54b06d4c7a5ca66df4c01857ac4
dkrzyzak <damntheguitars@gmail.com>
2024-08-09 22:30:08 +0200

    2015/day2

---
+++ src/y2015/day2/mod.rs
@@ -0,0 +1,35 @@
+use std::cmp::{max, min};
+
+use crate::read::read_input_file;
+
+pub fn task1() {
+    let lines = read_input_file("y2015", "day2");
+
+    let formatted: u32 = lines
+        .iter()
+        .map(|line| {
+            let dims: Vec<u32> = line
+                .split("x")
+                .map(|num_str| num_str.parse::<u32>().unwrap())
+                .collect();
+
+            let (l, w, h) = (dims[0], dims[1], dims[2]);
+
+            let a1 = l * w;
+            let a2 = l * h;
+            let a3 = w * h;
+            let smallest_area = min(a1, min(a2, a3));
+
+            let total_area = 2 * a1 + 2 * a2 + 2 * a3 + smallest_area; // task 1
+
+            let volume = l * w * h;
+            let smallest_perimeter = 2 * (l + w + h - max(l, max(w, h)));
+
+            let ribbon_length = smallest_perimeter + volume;
+
+            return ribbon_length;
+        })
+        .sum();
+
+    println!("Sum of all: {formatted}");
+}
