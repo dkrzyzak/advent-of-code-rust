fae4ea3afffcb0cd079dc2520e5293f37a3887d3
dkrzyzak <damntheguitars@gmail.com>
2024-08-21 22:49:07 +0200

    2023 day 13 part 2

--- src/y2023/day13/helpers.rs
+++ src/y2023/day13/helpers.rs
@@ -26,13 +26,29 @@ pub fn print_mirror(mirror: &Vec<Vec<char>>) {
     println!("");
 }
 
+pub fn possible_horizontal_smudge(row1: &Vec<char>, row2: &Vec<char>) -> bool {
+    let diffs: u8 = row1
+        .iter()
+        .zip(row2)
+        .map(|(current, next)| if current == next { 0 } else { 1 })
+        .sum();
+
+    return diffs == 1;
+}
+
 pub fn horizontal_index(mirror: &Vec<Vec<char>>) -> Option<usize> {
     for row in 0..mirror.len() - 1 {
+        // FOR PART 1 change to 0
+        let mut smudges_left = 1;
         let current_row = &mirror[row];
         let next_row = &mirror[row + 1];
 
         if current_row != next_row {
-            continue;
+            if possible_horizontal_smudge(current_row, next_row) {
+                smudges_left -= 1;
+            } else {
+                continue;
+            }
         }
         let mut search_back = row as isize - 1;
         let mut search_forward = row + 2;
@@ -40,14 +56,23 @@ pub fn horizontal_index(mirror: &Vec<Vec<char>>) -> Option<usize> {
 
         'mirror_verification: while search_back >= 0 && search_forward < mirror.len() {
             if &mirror[search_back as usize] != &mirror[search_forward] {
-                search_success = false;
-                break 'mirror_verification;
+                if smudges_left > 0
+                    && possible_horizontal_smudge(
+                        &mirror[search_back as usize],
+                        &mirror[search_forward],
+                    )
+                {
+                    smudges_left -= 1;
+                } else {
+                    search_success = false;
+                    break 'mirror_verification;
+                }
             }
             search_back -= 1;
             search_forward += 1;
         }
 
-        if search_success {
+        if search_success && smudges_left == 0 {
             return Some(row + 1); // enumerating from 1;
         }
     }
@@ -59,15 +84,15 @@ pub fn vertical_index(mirror: &Vec<Vec<char>>) -> Option<usize> {
     let cols_amount = mirror[0].len();
 
     'outer: for col in 0..cols_amount - 1 {
-        let mut was_col_mirrored = true;
-        // let mut smudges_left = 1;
+        // FOR PART 1 change to 0
+        let mut smudges_left = 1;
 
         for row in mirror {
             if row[col] != row[col + 1] {
-                // if smudges_left == 0 {
+                if smudges_left == 0 {
                     continue 'outer;
-                // }
-                // smudges_left -= 1;
+                }
+                smudges_left -= 1;
             }
         }
 
@@ -78,8 +103,12 @@ pub fn vertical_index(mirror: &Vec<Vec<char>>) -> Option<usize> {
         'mirror_verification: while search_back >= 0 && search_forward < cols_amount {
             for row in mirror {
                 if row[search_back as usize] != row[search_forward] {
-                    search_success = false;
-                    break 'mirror_verification;
+                    if smudges_left > 0 {
+                        smudges_left -= 1;
+                    } else {
+                        search_success = false;
+                        break 'mirror_verification;
+                    }
                 }
             }
 
@@ -87,7 +116,7 @@ pub fn vertical_index(mirror: &Vec<Vec<char>>) -> Option<usize> {
             search_forward += 1;
         }
 
-        if search_success {
+        if search_success && smudges_left == 0 {
             return Some(col + 1); // enumerating from 1
         }
     }
