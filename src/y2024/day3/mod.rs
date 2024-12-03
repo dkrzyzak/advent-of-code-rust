a1ce7cde64f16d65ca478376ceea44496c3fe5da
dkrzyzak <damntheguitars@gmail.com>
2024-12-03 23:04:59 +0100

    2024/3 prettify

--- src/y2024/day3/mod.rs
+++ src/y2024/day3/mod.rs
@@ -1,79 +1,30 @@
 use regex::Regex;
-
 use crate::parse_input;
-
-
-#[derive(Debug)]
-enum Instruction {
-    Multiply(usize, usize),
-    Enable,
-    Disable,
-}
 
 pub fn task() {
     let lines = parse_input!();
+    let memory = lines.join("");
 
-    // the entry data is split over multiple lines, 
-    // so we need to keep the global counter as well as global "enabled" state
-    // and update it after processing each another line
-    let mut total = 0usize;
+    let mut total = 0u32;
     let mut enabled = true;
 
-    let mul_reg = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let enable_reg = Regex::new(r"do\(\)").unwrap();
-    let disable_reg = Regex::new(r"don't\(\)").unwrap();
-    
-    for line in lines.iter() {
-        let mut instructions: Vec<(usize, Instruction)> = Vec::new();
+    let reg = Regex::new(r"mul\((\d+),(\d+)\)|do(?:n't)?\(\)").unwrap();
 
-        // parse mul(X,Y) instructions 
-        for captures in mul_reg.captures_iter(line) {
-            let a_match = captures.get(1).unwrap();
-            let a_index = a_match.start();
-        
-            let a = a_match.as_str().parse::<usize>().unwrap();
-            let b = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
+    for captures in reg.captures_iter(&memory) {
+        let captured = captures.get(0).unwrap().as_str();
 
-            instructions.push((a_index, Instruction::Multiply(a, b)));
-        }
-
-        // parse do() instructions 
-        for matched in enable_reg.find_iter(line) {
-            instructions.push((matched.start(), Instruction::Enable));
-        }
-
-        // parse don't() instructions 
-        for matched in disable_reg.find_iter(line) {
-            instructions.push((matched.start(), Instruction::Disable));
-        }
-
-        instructions.sort_by_key(|(index, _)| *index);
-
-        let (parsed_line, new_enabled) = parse_instructions(&instructions, enabled);
-
-        total += parsed_line;
-        enabled = new_enabled;
-
-    }
-
-    println!("Total: {}", total);
-}
-
-fn parse_instructions(instructions: &Vec<(usize, Instruction)>, mut enabled: bool) -> (usize, bool) {
-    let mut total = 0usize;
-    
-    for (_, instr) in instructions.iter() {
-        match *instr {
-            Instruction::Enable => { enabled = true },
-            Instruction::Disable => { enabled = false },
-            Instruction::Multiply(a, b) => {
+        match captured {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => {
                 if enabled {
+                    let a = captures.get(1).unwrap().as_str().parse::<u32>().unwrap();
+                    let b = captures.get(2).unwrap().as_str().parse::<u32>().unwrap();
                     total += a * b;
                 }
             }
-
         }
     }
 
-    (total, enabled)
+    println!("Total: {}", total);
 }
