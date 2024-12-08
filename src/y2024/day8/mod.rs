5ddae2eaf76b573b166d4d426eac6f830cb02acb
dkrzyzak <damntheguitars@gmail.com>
2024-12-08 22:49:14 +0100

    2024/8 part 2

--- src/y2024/day8/mod.rs
+++ src/y2024/day8/mod.rs
@@ -11,31 +11,9 @@ pub fn task() {
     let lines = parse_input!();
     let (grid, antennas_map) = extract_antennas(&lines);
 
-    let mut antinodes: HashSet<Point> = HashSet::new();
-    for (symbol, antennas) in antennas_map.iter() {
-        let pairs: Vec<(&Point, &Point)> = antennas
-            .iter()
-            .enumerate()
-            .flat_map(|(i, p1)| antennas.iter().skip(i + 1).map(move |p2| (p1, p2)))
-            .collect();
+    part1(&grid, &antennas_map);
 
-        for &(a1, a2) in pairs.iter() {
-            let d_vector = a2 - a1;
-            // println!("{:?} - {:?}, vector: {:?}", a1, a2, d_vector);
-            let antinode_1 = *a1 - d_vector;
-            let antinode_2 = *a2 + d_vector;
-
-            if let Some(symbol_at_node) = grid.get_point(&antinode_1) {
-                antinodes.insert(antinode_1);
-            }
-
-            if let Some(symbol_at_node) = grid.get_point(&antinode_2) {
-                antinodes.insert(antinode_2);
-            }
-        }
-    }
-
-    println!("Total: {}", antinodes.len());
+    part2(&grid, &antennas_map);
 }
 
 pub fn extract_antennas(lines: &Vec<String>) -> (Grid, Antennas) {
@@ -61,4 +39,61 @@ pub fn extract_antennas(lines: &Vec<String>) -> (Grid, Antennas) {
     }
 
     (Grid::initialize(grid), antennas)
+}
+
+pub fn part1(grid: &Grid, antennas_map: &Antennas) {
+    let mut antinodes: HashSet<Point> = HashSet::new();
+    for (symbol, antennas) in antennas_map.iter() {
+        let pairs: Vec<(&Point, &Point)> = antennas
+            .iter()
+            .enumerate()
+            .flat_map(|(i, p1)| antennas.iter().skip(i + 1).map(move |p2| (p1, p2)))
+            .collect();
+
+        for &(a1, a2) in pairs.iter() {
+            let d_vector = a2 - a1;
+
+            let antinode_backward = *a1 - d_vector;
+            let antinode_forward = *a2 + d_vector;
+
+            if grid.contains_point(&antinode_backward) {
+                antinodes.insert(antinode_backward);
+            }
+
+            if grid.contains_point(&antinode_forward) {
+                antinodes.insert(antinode_forward);
+            }
+        }
+    }
+
+    println!("Part 1: {}", antinodes.len());
+}
+
+pub fn part2(grid: &Grid, antennas_map: &Antennas) {
+    let mut antinodes: HashSet<Point> = HashSet::new();
+    for (symbol, antennas) in antennas_map.iter() {
+        let pairs: Vec<(&Point, &Point)> = antennas
+            .iter()
+            .enumerate()
+            .flat_map(|(i, p1)| antennas.iter().skip(i + 1).map(move |p2| (p1, p2)))
+            .collect();
+
+        for &(a1, a2) in pairs.iter() {
+            let d_vector = a2 - a1;
+
+            let mut antinodes_backward = *a1;
+            while grid.contains_point(&antinodes_backward) {
+                antinodes.insert(antinodes_backward);
+                antinodes_backward = antinodes_backward - d_vector;
+            }
+
+            let mut antinodes_forward = *a2;
+            while grid.contains_point(&antinodes_forward) {
+                antinodes.insert(antinodes_forward);
+                antinodes_forward = antinodes_forward + d_vector;
+            }
+        }
+    }
+
+    println!("Part 2: {}", antinodes.len());
 }
